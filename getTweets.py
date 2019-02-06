c267e4d1d6c2d29e9d142c0771acda587413f05a
murchant <mamurtag@tcd.ie>
2019-02-06 15:30:41 +0000

    clean up

--- getTweets.py
+++ getTweets.py
@@ -1,16 +1,19 @@
 import os
 import relations
+import pandas as pd
 
 def main():
-    relations.make_commands()
+    # relations.make_commands()
+    # combine_tweets()
     print("Commands made")
 
-if __name__ == '__main__':
-    main()
-
+    
 # Combine long & query tweet returned from API call
 def combine_tweets():
     df_long = pd.read_csv("info/true.csv", sep=';', error_bad_lines=False)
     df_short = pd.read_csv("info/trueShorter.csv", sep=';', error_bad_lines=False)
     df_left_outer = pd.merge(df_short, df_long, on='text', how="left")
     df_left_outer.to_csv("info/true_data_set.csv")
+
+if __name__ == '__main__':
+    main()
