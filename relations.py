3b1b767081ff49a96c4772b609d55c25f4ec38a8
murchant <mamurtag@tcd.ie>
2019-02-06 14:52:25 +0000

    directory cleanup

--- relations.py
+++ relations.py
@@ -7,7 +7,7 @@ def main():
 
 
 def get_true_rels():
-    with open('relationships.csv', mode='w') as rels:
+    with open('info/relationships.csv', mode='w') as rels:
         rel_writer = csv.writer(rels, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         with open('info/transfers.csv') as transfers:
             csv_reader = csv.reader(transfers, delimiter=',')
@@ -17,7 +17,7 @@ def get_true_rels():
 
 def make_true_qterms(type):
     true_queries = []
-    with open('relationships.csv') as transfers:
+    with open('info/relationships.csv') as transfers:
         csv_reader = csv.reader(transfers, delimiter=',')
         for row in csv_reader:
             true_queries.append(generate_ways(row, type))
@@ -39,24 +39,24 @@ def generate_ways(transfer, type):
     return variations
 
 def make_commands():
-    file = open("cmds.txt",'w')
+    file = open("twitter_queries/cmds.txt",'w')
     player_rels = make_true_qterms("0")
     wait_break=0
     for i in player_rels:
         for j in i:
             term = '"' + j + '"'
-            cmd = "python GetOldTweets/Exporter.py --querysearch " + term + " --maxtweets 100 --since 2018-06-01 --until 2018-08-30  --output true.csv + &"
+            cmd = "python GetOldTweets/Exporter.py --querysearch " + term + " --maxtweets 100 --since 2018-06-01 --until 2018-08-30  --output info/true.csv + &"
             file.write(cmd+"\n")
             wait_break=wait_break+1
             if wait_break%20==0:
                 file.write("wait"+"\n")
     file.close()
-    file = open("cmdsShorter.txt",'w')
+    file = open("twitter_queries/cmdsShorter.txt",'w')
     player_rels = make_true_qterms("1")
     for i in player_rels:
         for j in i:
             term = '"' + j + '"'
-            cmd = "python GetOldTweets/Exporter.py --querysearch " + term + " --maxtweets 100 --since 2018-06-01 --until 2018-08-30  --output trueShorter.csv + &"
+            cmd = "python GetOldTweets/Exporter.py --querysearch " + term + " --maxtweets 100 --since 2018-06-01 --until 2018-08-30  --output info/trueShorter.csv + &"
             file.write(cmd+"\n")
             wait_break=wait_break+1
             if wait_break%20==0:
