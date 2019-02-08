0822769bced0345503c836336bbd2b4260860267
murchant <mamurtag@tcd.ie>
2019-02-08 14:18:51 +0000

    NER beginning

--- docs/links.md
+++ docs/links.md
@@ -1,6 +1,13 @@
-# Links
+## Links
 
-### Named entity Recognition
+#### Named entity Recognition
+
 [NLTK](https://towardsdatascience.com/named-entity-recognition-with-nltk-and-spacy-8c4a7d88e7da)
 
+[SpaCy](https://spacy.io/usage/)
+
 [NER Stanford](https://nlp.stanford.edu/software/CRF-NER.shtml)
+
+
+#### Ascii's
+- ’ 
---
+++ ner.py
@@ -0,0 +1,41 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+from nltk.chunk import conlltags2tree, tree2conlltags, ne_chunk
+from pprint import pprint
+import spacy
+from spacy import displacy
+from collections import Counter
+import en_core_web_sm
+
+def main():
+    # nltk.download()
+    # nltk.download('averaged_perceptron_tagger')
+    # nltk.download('maxent_ne_chunker')
+    # nltk.download('words')
+    ex = "Leicester keen on Portos Pereira: Leicester City are interested in signing Porto right back Ricardo Pereira , Sky Sports News has learned, as Claude Puel considers options to strengthen his defence this summer"
+    u = unicode(ex, 'utf-8')
+    pattern = 'NP: {<DT>?<JJ>*<NN>}'
+    sent = preprocess(ex)
+    cp = nltk.RegexpParser(pattern)
+    cs = cp.parse(sent)
+    iob_tagged = tree2conlltags(cs)
+    ne_tree = ne_chunk(pos_tag(word_tokenize(ex)))
+    # print(ne_tree)
+
+    # SPACEY ####
+
+    nlp = en_core_web_sm.load()
+    doc = nlp(u)
+    pprint([(X.text, X.label_) for X in doc.ents])
+
+    # SPACEY ####
+
+
+def preprocess(sent):
+    sent = nltk.word_tokenize(sent)
+    sent = nltk.pos_tag(sent)
+    return sent
+
+if __name__ == '__main__':
+    main()
