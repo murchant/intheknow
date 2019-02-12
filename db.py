14bdeaf29a7952f8adea037b1cdd326978074f19
murchant <mamurtag@tcd.ie>
2019-02-12 11:46:08 +0000

    cleanup

--- db.py
+++ db.py
@@ -5,20 +5,20 @@ import pandas as pd
 def main():
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     transferdb = myclient["transferdb"]
+    relsdb = myclient["truetransfers"]
     dblist = myclient.list_database_names()
-    # store_true(transferdb)
+    # path = "info/transfers2018.csv"
+    # store_true(transferdb, path)
     query = {"username":"SoccerTube2"}
     query_collection(query, "true_transfers")
-
-
 
     if "mydatabase" in dblist:
         print("The database exists.")
 
 
-def store_true(transferdb):
+def store_true(transferdb, path):
     mycol = transferdb["true_transfers"]
-    df_transfer_true = pd.read_csv("info/true_data_set.csv", sep=';', error_bad_lines=False, encoding="utf-8-sig")
+    df_transfer_true = pd.read_csv(path, sep=';', error_bad_lines=False, encoding="utf-8-sig")
     for i, row in df_transfer_true.iterrows():
         entry = {"username": row['username'], "date": row['date'], "text": row['text']}
         mycol.insert_one(entry)
