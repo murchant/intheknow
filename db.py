fa4266715271488ca6de9756c93eed87a6a64e77
murchant <mamurtag@tcd.ie>
2019-02-13 15:30:58 +0000

    ner processing start

--- db.py
+++ db.py
@@ -1,19 +1,21 @@
 import pymongo
 from pymongo import MongoClient
 import pandas as pd
+from pprint import pprint
 
 def main():
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     transferdb = myclient["transferdb"]
-    print(transferdb.collection_names())
     # DATABASE SETUP
     # path = "info/true_data_set.csv"
-    path2 = "info/transfers2018.csv"
+    # path2 = "info/transfers2018.csv"
     # store_data(transferdb, path)
     # make_player_db(transferdb, path2)
-    make_transfer_db(transferdb, path2)
-    # query = {"player":"Carl Baker"}
-    # query_collection(query, "players", transferdb)
+    # make_transfer_db(transferdb, path2)
+    query = {"player": "Carl Baker"}
+    x = query_collection(query, "players", transferdb)
+
+
 
 
 def store_data(transferdb, path):
@@ -24,15 +26,15 @@ def store_data(transferdb, path):
         mycol.insert_one(entry)
     return
 
+
 def make_player_db(transferdb, path):
     coll = transferdb["players"]
     df_transfer_true = pd.read_csv(path, sep=',', error_bad_lines=False, encoding="utf-8")
     for i, row in df_transfer_true.iterrows():
-        print(row['Name'])
-        print("----------")
         entry = {"player": row['Name'].strip(), "date": row["Date"]}
         coll.insert_one(entry)
     return
+
 
 def make_club_db(transferdb, path):
     coll = transferdb["clubs"]
@@ -44,20 +46,22 @@ def make_club_db(transferdb, path):
         coll.insert_one(entry2)
     return
 
+
 def make_transfer_db(transferdb, path):
     coll = transferdb["confirmed_transfers"]
     df_transfer_true = pd.read_csv(path, sep=',', error_bad_lines=False, encoding="utf-8")
     for i, row in df_transfer_true.iterrows():
-        entry = {"Date": row["Date"].strip(),"Name": row["Name"].strip(),"Moving from": row["Moving from"].strip(),"Moving to": row["Moving to"].strip()}
+        entry = {"Date": row["Date"].strip(),"Name": row["Name"].strip(),"Moving from": row["Moving from"].strip(),"Moving to": row["Moving to"].strip(), "happened": True}
         coll.insert_one(entry)
     return
+
 
 def query_collection(query, cname, db):
     collection=db[cname]
     docs = collection.find(query)
     for i in docs:
         print(i)
-    return
+    return docs
 
 
 if __name__ == '__main__':
--- ner.py
+++ ner.py
@@ -7,16 +7,61 @@ import spacy
 from spacy import displacy
 from collections import Counter
 import en_core_web_sm
+import pymongo
+from pymongo import MongoClient
+import db
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+transferdb = myclient["transferdb"]
 
 def main():
-    # SPACEY #####
-    ex = "Leicester keen on Portos Pereira: Leicester City are interested in signing Porto right back Ricardo Pereira , Sky Sports News has learned, as Claude Puel considers options to strengthen his defence this summer"
-    u = unicode(ex, 'utf-8')
+    tweet = "Leicester keen on Portos Pereira: Leicester City are interested in signing Porto right back Ricardo Pereira , Sky Sports News has learned, as Claude Puel considers options to strengthen his defence this summer"
+    entities = get_entities(tweet)
+    print(entities)
+    pplayers = get_potential_players(entities)
+    print(pplayers)
+    x = make_player_queries(pplayers)
+    test = query_db(x)
+    print test
+
+def get_potential_players(ents):
+    potentials=[]
+    for (i,j) in ents:
+        if j == "PERSON":
+            potentials.append(i)
+    return potentials
+
+
+def preprocess(sent):
+    sent = nltk.word_tokenize(sent)
+    sent = nltk.pos_tag(sent)
+    return sent
+
+def get_entities(text):
+    ents=[]
+    u = unicode(text, 'utf-8')
     nlp = en_core_web_sm.load()
     doc = nlp(u)
-    pprint([(X.text, X.label_) for X in doc.ents])
+    # pprint([(X.text, X.label_) for X in doc.ents])
+    for i in doc.ents:
+        ents.append((i.text, i.label_))
+    return ents
 
-    # SPACEY #####
+def make_player_queries(names):
+    querys=[]
+    for i in names:
+        q = {"Name": i}
+        querys.append(q)
+    return querys
+
+def query_db(qs):
+    actual_player=[]
+    for i in qs:
+        x = db.query_collection(i, "confirmed_transfers", transferdb)
+        if x.count() > 0:
+            actual_player.append((i, x))
+    return actual_player
+
 
 def nltk_method(str):
     # INSTALL
@@ -34,11 +79,6 @@ def nltk_method(str):
     iob_tagged = tree2conlltags(cs)
     ne_tree = ne_chunk(pos_tag(word_tokenize(ex)))
     print(ne_tree)
-
-def preprocess(sent):
-    sent = nltk.word_tokenize(sent)
-    sent = nltk.pos_tag(sent)
-    return sent
 
 if __name__ == '__main__':
     main()
