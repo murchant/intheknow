3eb227968998cd14228f38c9b6d0e27b9facbe44
murchant <mamurtag@tcd.ie>
2019-02-13 13:57:29 +0000

    clubdb created

--- db.py
+++ db.py
@@ -5,16 +5,15 @@ import pandas as pd
 def main():
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     transferdb = myclient["transferdb"]
-    dblist = myclient.list_database_names()
-    print (transferdb.collection_names())
-    path = "info/true_data_set.csv"
+    print(transferdb.collection_names())
+    # DATABASE SETUP
+    # path = "info/true_data_set.csv"
     path2 = "info/transfers2018.csv"
     # store_data(transferdb, path)
     # make_player_db(transferdb, path2)
-    query = {"player":"Carl Baker"}
-    query_collection(query, "players", transferdb)
-    if "mydatabase" in dblist:
-        print("The database exists.")
+    make_club_db(transferdb, path2)
+    # query = {"player":"Carl Baker"}
+    # query_collection(query, "players", transferdb)
 
 
 def store_data(transferdb, path):
@@ -33,6 +32,16 @@ def make_player_db(transferdb, path):
         print("----------")
         entry = {"player": row['Name'].strip(), "date": row["Date"]}
         coll.insert_one(entry)
+    return
+
+def make_club_db(transferdb, path):
+    coll = transferdb["clubs"]
+    df_transfer_true = pd.read_csv(path, sep=',', error_bad_lines=False, encoding="utf-8")
+    for i, row in df_transfer_true.iterrows():
+        entry1 = {"Moving from": row['Name'].strip()}
+        entry2 = {"Moving to": row['Name'].strip()}
+        coll.insert_one(entry1)
+        coll.insert_one(entry2)
     return
 
 
